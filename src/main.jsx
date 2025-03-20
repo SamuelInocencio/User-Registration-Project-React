6c53e16ec746b2f7299ac7e46c6055c45c13b26e
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-03-20 11:19:33 -0300

    UseRef implemented

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 
-import App from './App.jsx'
+import Home from './pages/Home'
 import GlobalStyles from './styles/GlobalStyles.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalStyles />
-    <App />
+    <Home />
   </StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,51 @@
+import { useRef } from 'react'
+
+import UsersImage from '../../assets/users.png'
+import { Button, Container, ContainerInputs, Form, Input, InputLabel, Title, TopBackground } from './styles'
+
+function Home() {
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function registerNewUser() {
+    const name = inputName.current.value
+    const age = inputAge.current.value
+    const email = inputEmail.current.value
+
+    console.log(name, age, email)
+  }
+
+
+  return (
+    <Container>
+      <TopBackground>
+        <img src={UsersImage} alt="Imagem de usuários" />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastrar Usuários</Title>
+
+        <ContainerInputs>
+          <div>
+            <InputLabel>Nome <span>*</span></InputLabel>
+            <Input type='text' placeholder='Nome do usuário' ref={inputName} />
+
+            <InputLabel>Idade <span>*</span></InputLabel>
+            <Input type='number' placeholder='Idade do usuário' ref={inputAge} />
+          </div>
+        </ContainerInputs>
+        <div style={{width: '100%'}}>
+          <InputLabel>Email <span>*</span></InputLabel>
+          <Input type='email' placeholder='Email do usuário' ref={inputEmail} />
+
+        </div>
+
+        <Button type='button' onClick={registerNewUser}>Cadastrar Usuário</Button>
+      </Form>
+    </Container >
+  )
+}
+
+export default Home
