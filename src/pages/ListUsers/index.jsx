e1a69ad9c8423f8bf3f87db57fb6ba38776ecf51
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-03-22 07:52:07 -0300

    implemented UseEffect

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,15 +1,25 @@
+import { use, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import api from '../../services/api';
 
 import DefaultButton from '../../components/Button';
 import TopBackground from '../../components/TopBackground';
 
 function ListUsers() {
+  useEffect(() => {
+    async function getUsers() {
+      const usersFromAPI = await api.get('/usuarios');
+      console.log(usersFromAPI.data);
+    }
+    getUsers();
+  }, []);
 
   const navigate = useNavigate();
 
   return (
     <>
       <TopBackground />
+      <h1>Listagem de Usuários</h1>
       <DefaultButton onClick={() => navigate('/')}>Voltar</DefaultButton>
     </>
   );
